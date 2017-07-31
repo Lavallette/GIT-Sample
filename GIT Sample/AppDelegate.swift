1b5913e0deee6794c8aa0f7f5981c1466cc25e2b
Lavallette <dvallet@gmail.com>
2017-07-31 15:55:20 +0200

    Initial Commit

---
+++ GIT Sample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GIT Sample
+//
+//  Created by Didier Vallet on 31.07.17.
+//  Copyright © 2017 Didier Vallet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GIT Sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GIT Sample
+//
+//  Created by Didier Vallet on 31.07.17.
+//  Copyright © 2017 Didier Vallet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
